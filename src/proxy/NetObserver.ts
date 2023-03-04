9689088f136f868b86ccfa816e6cd716e438db37
custer <custer@xx.xx>
2023-03-04 12:15:27 +0900

    如果没有收到配置信息，则抛出错误

--- src/proxy/NetObserver.ts
+++ src/proxy/NetObserver.ts
@@ -38,8 +38,12 @@ export default class NetObserver extends AbstractMediator {
         const selfProxy: SelfProxy = this.getProxy(SelfProxy);
         const settingProxy: SettingProxy = getProxy(SettingProxy);
         switch (notification.getName()) {
+            // 平台配置信息
             case net.EventType.api_config:
                 PlatConfig.config = body;
+                if(!body || !body.client){
+                    alert("config error");
+                }
                 GlobalVar.plat_id = PlatConfig.config.client.plat_id;
                 GlobalVar.cdnUrl = PlatConfig.config.client.cdn_url;
                 // 如果没有配置，给默认值
@@ -70,6 +74,7 @@ export default class NetObserver extends AbstractMediator {
 
                 LangConfig.load(GlobalVar.lang);
                 break;
+            //语言包
             case NotificationName.LANG_CONFIG:
                 if (GlobalVar.token) {
                     this.facade.sendNotification(net.HttpType.api_user_info);
@@ -79,6 +84,7 @@ export default class NetObserver extends AbstractMediator {
                     orderUnsettledProxy.api_user_orders();
                 }
                 break;
+            //用户信息
             case net.EventType.api_user_info:
                 selfProxy.set_user_info(body);
                 settingProxy.resetForm();
@@ -86,19 +92,23 @@ export default class NetObserver extends AbstractMediator {
                 window["vm"].$mount("#app");
                 GlobalVar.loading = false;
                 break;
+            //体育种类
             case net.EventType.api_event_sports:
                 PlatConfig.allSprots = body;
                 break;
+            //所有盘口类型
             case net.EventType.api_event_market_type_v2:
                 PlatConfig.allMarketType = body.market_type;
                 PlatConfig.market_main_type = body.market_main_type;
                 break;
+            //盘口
             case net.EventType.api_market_typelist:
                 if (type == BetProxy.NAME) {
                     const betProxy: BetProxy = getProxy(BetProxy);
                     betProxy.set_market_typelist(body);
                 }
                 break;
+            //赛事进程
             case net.EventType.api_event_states:
                 if (type == BetProxy.NAME) {
                     const betProxy: BetProxy = getProxy(BetProxy);
@@ -109,15 +119,18 @@ export default class NetObserver extends AbstractMediator {
                     orderUnsettledProxy.set_event_states(body);
                 }
                 break;
+            //设置成功后，重新获取用户信息
             case net.EventType.api_user_set_user_setting:
                 selfProxy.api_user_info();
                 break;
+            //订单
             case net.EventType.api_user_orders:
                 if (type == OrderUnsettledProxy.NAME) {
                     const orderUnsettledProxy: OrderUnsettledProxy = getProxy(OrderUnsettledProxy);
                     orderUnsettledProxy.set_user_orders(body);
                 }
                 break;
+            //投注返回结果
             case net.EventType.api_user_betfix:
                 {
                     // GlobalVar.loading = false;
@@ -129,7 +142,7 @@ export default class NetObserver extends AbstractMediator {
                 break;
             case net.EventType.REQUEST_END:
                 {
-                    switch(body.config.url){
+                    switch (body.config.url) {
                         case net.HttpType.api_user_betfix:
                             const betProxy: BetProxy = getProxy(BetProxy);
                             betProxy.pageData.loading = false;
