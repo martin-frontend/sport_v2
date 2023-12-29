a3f4c669d3dcab2815584d74502e07edafa5be37
martin <martin.yu@baiyitech.com.tw>
2023-12-29 10:33:49 +0800

    dev

--- src/_skin001/views/live_list/index.ts
+++ src/_skin001/views/live_list/index.ts
@@ -1,12 +1,19 @@
 import getProxy from "@/core/global/getProxy";
 import LiveListProxy from "./proxy/LiveListProxy";
 import LiveProxy from "../live/proxy/LiveProxy";
+import Vue from "vue";
+import PageHomeProxy from "../page_home/proxy/PageHomeProxy";
 
 function show() {
     const myProxy: LiveListProxy = getProxy(LiveListProxy);
-    const liveProxy: LiveProxy = getProxy(LiveProxy);
     myProxy.pageData.isOpen = true;
-    myProxy.listQueryComp.sport_id = liveProxy.listQueryComp.sport_id;
+    if (Vue.vuetify.framework.breakpoint.mobile) {
+        const homeProxy: PageHomeProxy = getProxy(PageHomeProxy);
+        myProxy.listQueryComp.sport_id = homeProxy.listQueryComp.sport_id;
+    } else {
+        const liveProxy: LiveProxy = getProxy(LiveProxy);
+        myProxy.listQueryComp.sport_id = liveProxy.listQueryComp.sport_id;
+    }
     myProxy.api_event_live_list_v2();
 }
 
