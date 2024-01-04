3780d53d22030d0e918844fa962a2e2c464be7e6
custer <custer@xx.xx>
2024-01-04 19:43:57 +0900

    网络错误提示

--- src/_skin001/command/RequestErrorCMD.ts
+++ src/_skin001/command/RequestErrorCMD.ts
@@ -1,5 +1,14 @@
+import dialog_message_box from "../views/dialog_message_box";
+import LangUtil from "@/core/global/LangUtil";
+
 export default class RequestErrorCMD extends puremvc.SimpleCommand {
     execute(notification: puremvc.INotification) {
         const body = notification.getBody();
+        dialog_message_box.alert({
+            message: LangUtil("请检查网络"),
+            okFun: () => {
+                location.reload();
+            },
+        });
     }
 }
