4fa3e8a350b9674a82167013cbe7a9adb66ccb41
martin <martin.yu@baiyitech.com.tw>
2024-01-09 18:19:40 +0800

    dev(todo onReverse)

--- src/_skin001/views/page_home/widget/event_filter/EventFilter.ts
+++ src/_skin001/views/page_home/widget/event_filter/EventFilter.ts
@@ -15,11 +15,95 @@ export default class EventFilter extends AbstractView {
     myProxy: PageHomeProxy = this.getProxy(PageHomeProxy);
     pageData = this.myProxy.pageData;
     listQueryComp = this.myProxy.listQueryComp;
-    selectAll = false;
+    selectAll = true;
     selectReverse = false;
-    panel = [0, 1];
+    selectCountry: any = [];
+    selectCompetion: any = {};
+    allCompetition: any = {}; // 原数据 对比用
+    indeterminates: any = {};
+    panel: number[] = [];
+
+    created() {
+        this.curSportNav?.all_competition.forEach((item: any, index: number) => {
+            this.panel.push(index);
+            this.selectCountry.push(item.country_code);
+            this.$set(this.selectCompetion, item.country_code, <any>[]);
+            this.$set(this.allCompetition, item.country_code, <any>[]);
+            this.$set(this.indeterminates, item.country_code, false);
+
+            item.competitions.forEach((comp: any) => {
+                this.selectCompetion[item.country_code].push(comp.id);
+                this.allCompetition[item.country_code].push(comp.id);
+            });
+        });
+    }
 
     onSave() {
         this.pageData.isShowFilter = false;
+    }
+
+    onReverse() {
+        this.curSportNav?.all_competition.forEach((item: any) => {
+            const arr: any = [];
+            item.competitions.forEach((comp: any) => {
+                if (!this.selectCompetion[item.country_code].includes(comp.id)) {
+                    arr.push(comp.id);
+                }
+            });
+            this.selectCompetion[item.country_code] = arr;
+        });
+    }
+
+    onCountryClick(country_code: any) {
+        if (this.selectCountry.includes(country_code)) {
+            this.selectCompetion[country_code] = [...this.allCompetition[country_code]];
+        } else {
+            this.selectCompetion[country_code] = [];
+        }
+        this.indeterminates[country_code] = false;
+    }
+
+    onCompetionClick(country_code: any) {
+        const length = this.selectCompetion[country_code].length;
+        const totalLength = this.allCompetition[country_code].length;
+
+        if (length == 0) {
+            this.indeterminates[country_code] = false;
+            this.selectCountry = this.selectCountry.filter((item: any) => item != country_code);
+        } else {
+            if (length == totalLength) {
+                this.indeterminates[country_code] = false;
+                if (!this.selectCountry.includes(country_code)) {
+                    this.selectCountry.push(country_code);
+                }
+            } else {
+                if (!this.selectCountry.includes(country_code)) {
+                    this.selectCountry.push(country_code);
+                }
+                this.indeterminates[country_code] = true;
+            }
+        }
+    }
+
+    get curSportId() {
+        return this.myProxy.listQueryComp.sport_id;
+    }
+
+    get curSportNav() {
+        return this.navProxy.pageData.new_menu_subnav[this.curSportId];
+    }
+
+    onAll(val: boolean) {
+        this.selectCountry = [];
+        if (val) {
+            Object.keys(this.allCompetition).forEach((country_code) => {
+                this.selectCountry.push(country_code);
+                this.selectCompetion[country_code] = [...this.allCompetition[country_code]];
+            });
+        } else {
+            Object.keys(this.allCompetition).forEach((country_code) => {
+                this.selectCompetion[country_code] = [];
+            });
+        }
     }
 }
