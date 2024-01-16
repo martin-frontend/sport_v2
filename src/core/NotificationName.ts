b154617f4577baadfa6d195f7dc4e22837f142ff
nico <nico@gmail.com>
2024-01-16 14:50:38 +0800

    6409 报错修改

--- src/core/NotificationName.ts
+++ src/core/NotificationName.ts
@@ -3,4 +3,6 @@ export default class NotificationName {
     static GAME_CONFIG = "GAME_CONFIG";
     /**语言包加载完成 */
     static LANG_CONFIG = "LANG_CONFIG";
+    /**投注消息状态不正确 */
+    static BET_FIX_ERROR = "BET_FIX_ERROR";
 }
