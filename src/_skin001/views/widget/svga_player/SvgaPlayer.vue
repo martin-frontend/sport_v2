8e826dfb0818b7210539568898e7290337e45ec3
martin <martin@gmail.com>
2024-02-06 10:14:57 +0800

    H5-6457svga

---
+++ src/_skin001/views/widget/svga_player/SvgaPlayer.vue
@@ -0,0 +1,10 @@
+<template src="./SvgaPlayer.vue.html" lang="html"></template>
+<style src="./SvgaPlayer.vue.scss" lang="scss" scoped></style>
+
+<script lang="ts">
+import Component from "vue-class-component";
+import SvgaPlayer from "./SvgaPlayer";
+
+@Component
+export default class extends SvgaPlayer {}
+</script>
